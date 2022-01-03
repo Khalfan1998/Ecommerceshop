c9b9848e8c768dcebb05ecf7534d4b833c6e76ee
Khalfan1998 <khalfan.ikns@gmail.com>
2022-01-03 16:31:34 +0300

    Products and Categories
    Adding Products and Categories

--- src/data.js
+++ src/data.js
@@ -9,7 +9,7 @@ export const sliderItems = [
 
   {
     id: 2,
-    img: "https://i.postimg.cc/BQ0M9Thg/felipe-galvan-Ahfr-A5-VQNp-M-unsplash-removebg-preview.png",
+    img: "https://i.postimg.cc/zfdqNqZk/daniel-lincoln-KADEo-Ll95-Gk-unsplash-removebg-preview.png",
     title: "AUTUMN COLLECTION",
     desc: "DONT MISS OUT ON OUR SALE! 30% OFF!",
     bg: "fcf1ed",
@@ -17,9 +17,66 @@ export const sliderItems = [
 
   {
     id: 3,
-    img: "https://i.postimg.cc/BQ0M9Thg/felipe-galvan-Ahfr-A5-VQNp-M-unsplash-removebg-preview.png",
+    img: "https://i.postimg.cc/hGGqpNzQ/amber2-removebg-preview.png",
     title: "LOUNGWEAR LOVE",
     desc: "DONT MISS OUT ON OUR SALE! 30% OFF!",
     bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://i.postimg.cc/02WQxGYS/nimble-made-BKYe-LLB1-Ox-I-unsplash.jpg",
+    title: "SHIRT STYLE",
+  },
+
+  {
+    id: 2,
+    img: "https://i.postimg.cc/P5JC8VXK/amanda-vick-oh-Wf6-Yuz-OQk-unsplash.jpg",
+    title: "JACKET STYLE",
+  },
+
+  {
+    id: 3,
+    img: "https://i.postimg.cc/fTwGC607/hatnew.jpg",
+    title: "HAT STYLE",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: "https://i.postimg.cc/PxJHS97F/benjamin-rascoe-i-Nv-K2-Xmpx8o-unsplash.jpg",
+  },
+
+  {
+    id: 2,
+    img: "https://i.postimg.cc/PxJHS97F/benjamin-rascoe-i-Nv-K2-Xmpx8o-unsplash.jpg",
+  },
+
+  {
+    id: 3,
+    img: "https://i.postimg.cc/PxJHS97F/benjamin-rascoe-i-Nv-K2-Xmpx8o-unsplash.jpg",
+  },
+  {
+    id: 4,
+    img: "https://i.postimg.cc/SQX2svKr/tobias-tullius-Fg15-Ldqp-Wrs-unsplash.jpg",
+  },
+  {
+    id: 5,
+    img: "https://i.postimg.cc/SQX2svKr/tobias-tullius-Fg15-Ldqp-Wrs-unsplash.jpg",
+  },
+  {
+    id: 6,
+    img: "https://i.postimg.cc/SQX2svKr/tobias-tullius-Fg15-Ldqp-Wrs-unsplash.jpg",
+  },
+  {
+    id: 7,
+    img: "https://i.postimg.cc/8crSSWNR/yang-deng-2lo-Kxdi6-Hmo-unsplash.jpg",
+  },
+  {
+    id: 8,
+    img: "https://i.postimg.cc/8crSSWNR/yang-deng-2lo-Kxdi6-Hmo-unsplash.jpg",
   },
 ];
