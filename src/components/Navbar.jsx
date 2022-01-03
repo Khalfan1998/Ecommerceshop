a60465d1a272d562783c66819ccd6135f6351d90
Khalfan1998 <khalfan.ikns@gmail.com>
2022-01-03 15:08:15 +0300

    Home Page Slider
    adding home page first slider

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -69,7 +69,7 @@ const Navbar = () => {
                 <Search style={{color:"gray", fontSize:16}}/>
             </SearchContainer>
             </Left>
-            <Center><Logo>KHALFAN.</Logo></Center>
+            <Center><Logo>SHOP.</Logo></Center>
             <Right>
                 <MenuItem>REGISTER</MenuItem>
                 <MenuItem>SIGN IN</MenuItem>
---
+++ src/data.js
@@ -0,0 +1,25 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://i.postimg.cc/BQ0M9Thg/felipe-galvan-Ahfr-A5-VQNp-M-unsplash-removebg-preview.png",
+    title: "SUMMER SALE",
+    desc: "DONT MISS OUT ON OUR SALE! 30% OFF!",
+    bg: "f5fafd",
+  },
+
+  {
+    id: 2,
+    img: "https://i.postimg.cc/BQ0M9Thg/felipe-galvan-Ahfr-A5-VQNp-M-unsplash-removebg-preview.png",
+    title: "AUTUMN COLLECTION",
+    desc: "DONT MISS OUT ON OUR SALE! 30% OFF!",
+    bg: "fcf1ed",
+  },
+
+  {
+    id: 3,
+    img: "https://i.postimg.cc/BQ0M9Thg/felipe-galvan-Ahfr-A5-VQNp-M-unsplash-removebg-preview.png",
+    title: "LOUNGWEAR LOVE",
+    desc: "DONT MISS OUT ON OUR SALE! 30% OFF!",
+    bg: "fbf0f4",
+  },
+];
